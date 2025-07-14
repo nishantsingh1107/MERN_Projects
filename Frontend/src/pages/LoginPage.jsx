8ad13eee8b3c23c65b43470b597d904c3989a500
nishantsingh1107 <nishantsingh0311@gmail.com>
2025-07-14 22:24:02 +0530

    new

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router";
-import { Navbar } from "../components/Navbar"; 
+import { Navbar } from "../components/Navbar.jsx"; 
 
 
 const LoginPage =() =>{
--- Frontend/src/pages/SignupPage.jsx
+++ Frontend/src/pages/SignupPage.jsx
@@ -1,4 +1,4 @@
-import { Navbar } from "../components/Navbar";
+import { Navbar } from "../components/Navbar.jsx";
 import { Link, useNavigate } from "react-router";
 import React from "react";  
 
